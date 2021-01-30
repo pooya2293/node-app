755763a19384cfe0f17bd888471334f548a2c994
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-01-30 19:09:59 +0330

    ES6 code in js file not read in node and undefined process in chrome i cant understan !

---
+++ greet.js
@@ -0,0 +1,5 @@
+//the app when you say your first and last name say hello to you
+
+let firstname = process.argv[2];
+let lastname = process.argv[3];
+console.log('Hello ${firstname} ${lastname}!');
