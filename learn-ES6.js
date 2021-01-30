ba8a1a292606a075169fb282239db964c5d4cd79
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-01-30 22:25:26 +0330

    TypeError: Cannot read property addEventListener of null

--- learn-ES6.js
+++ learn-ES6.js
@@ -48,31 +48,47 @@
 //--------------------
 
 // greet();//can not acsess
-	let greet = function(){
-		console.log('hi');
-	}	
-	greet();//hi
-	{
-		// greet();//can not acsess
-		let greet = function(){
-			console.log('hello');
-		}
-		let x = 20;
-		{
-			let greet = function(){
-				console.log('hi there');
-			}
-		}
-	greet();//hello
+// 	let greet = function(){
+// 		console.log('hi');
+// 	}	
+// 	greet();//hi
+// 	{
+// 		// greet();//can not acsess
+// 		let greet = function(){
+// 			console.log('hello');
+// 		}
+// 		let x = 20;
+// 		{
+// 			let greet = function(){
+// 				console.log('hi there');
+// 			}
+// 		}
+// 	greet();//hello
 		
-	}
-	//console.log(x);//x is not defined
+// 	}
+// 	//console.log(x);//x is not defined
 	
-	{
-		let greet = function(){
-			console.log('hi Hello');
-		}
+// 	{
+// 		let greet = function(){
+// 			console.log('hi Hello');
+// 		}
 		
-	}
+// 	}
 
-greet();//hi
+// greet();//hi
+// 
+// --------------------
+// 
+// 
+
+// for(var i = 0;i < 3; i ++) {
+//    document.getElementById(i).addEventListener('click', function(){
+// 			console.log(i);
+// 	})
+// }
+
+for(var i = 0; i < 3; i ++){
+   document.getElementById(i).addEventListener('click', function(){
+       console.log(i);
+   })
+}
